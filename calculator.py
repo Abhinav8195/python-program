2753de2f982d9a0ae7237401fa5d89f9edf0dc58
Abhinav8195 <149519597+Abhinav8195@users.noreply.github.com>
2023-11-16 11:12:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+enter=input("symbol")
+n1=int(input("enter number"))
+n2=int(input("enter number"))
+if (enter=='+'):
+     print(n1+n2)
+elif enter=='-':
+     print(n1-n2)   
+elif enter=='*':
+       print(n1*n2)
+elif enter=='/':
+       print(n1/n2)
